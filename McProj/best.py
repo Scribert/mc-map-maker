221b34cdf6c06cbff51403490db5eb7f5d55b0f7
treel <treel@DESKTOP-I3O7IAM>
2022-09-24 11:47:49 -0600

    Deleted best.py and test.py

--- McProj/best.py
+++
@@ -1,3 +0,0 @@
-import editor as bob
-r = bob.ivec2(-1, 4)
-print(r.x, r.y)
--- McProj/test.py
+++
@@ -1,4 +0,0 @@
-import editor as bob
-r = bob.vec3(4.0, 3.0, 5.0)
-r.g = 500
-print(r.x, r.y, r.z, r.r, r.g, r.b, r.s, r.t, r.p)
